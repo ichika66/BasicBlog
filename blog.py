cc0588de92f5df58c82c9faaa0610cfd170c00ba
Chiyoka Ide <chiyoka_66@hotmail.com>
2017-08-26 22:50:57 +0200

    modify confirm.html and confirm handler

--- blog.py
+++ blog.py
@@ -1,6 +1,7 @@
 import os
 import webapp2
 import jinja2
+from string import letters
 
 from google.appengine.ext import db
 
@@ -21,12 +22,13 @@ class Handler(webapp2.RequestHandler):
 
 # give datastore a parent
 def blog_key(name = 'default'):
-	return db.key.from_path('blogs', name)
+    return db.Key.from_path('blogs', name)
 
 class Blog(db.Model):
 	title = db.StringProperty(required = True)
 	art = db.TextProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)	
+	last_modified = db.DateTimeProperty(auto_now = True)
 
 class MainPage(Handler):
 	def get(self):
@@ -34,31 +36,28 @@ class MainPage(Handler):
 		self.render('front.html', arts = arts)
 
 class NewPost(Handler):
-	def render_newpost(self, title="", art="", error=""):
-		self.render("newpost.html", title=title, art=art, error=error)
-
 	def get(self):
-		self.render_newpost()
+		self.render("newpost.html")
 
 	def post(self):
 		title = self.request.get("title")
 		art = self.request.get("art")
 
 		if title and art:
-			a = Blog(title = title, art = art)
+			a = Blog(parent = blog_key(), title = title, art = art)
 			a.put()
-
-			self.redirect("/")
+			self.redirect('/%s' % str(a.key().id()))
 
 		else:
 			error = "we need both parameteres!"
-			self.render_newpost(title, art, error)
+			self.render("newpost.html", title=title, art=art, error=error)
 
-class Confirm(webapp2.RequestHandler):
-	def get(self):
-		arts = Art()
-		arts.sent = self.request.get('')
+class Confirm(Handler):
+	def get(self, post_id):
+		key = db.Key.from_path('Blog', int(post_id), parent=blog_key())
+		post = db.get(key)
+		self.render("confirm.html", post = post)
 
 app = webapp2.WSGIApplication([('/', MainPage),
 								('/newpost', NewPost),
-								('/(\d+)', Confirm)], debug=True)
+								('/([0-9]+)', Confirm)], debug=True)
