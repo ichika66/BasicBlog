1ed2c7c4da80b00be11763cded1b32220d272ba6
Chiyoka Ide <chiyoka_66@hotmail.com>
2017-08-26 20:31:40 +0200

    delete commented out lines

--- blog.py
+++ blog.py
@@ -29,36 +29,12 @@ class Blog(db.Model):
 	created = db.DateTimeProperty(auto_now_add = True)	
 
 class MainPage(Handler):
-	#def render_front(self, title="", art=""):
-	#	arts = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC")
-	#	self.render("front.html", title=title, art=art, arts=arts)
-
-	#def get(self):
-	#	self.render_front()
-
 	def get(self):
 		arts = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC")
 		self.render('front.html', arts = arts)
 
-	# def post(self):
-	# 	title = self.request.get("title")
-	# 	art = self.request.get("art")
-
-	# 	if title and art:
-	# 		a = Blog(title = title, art = art)
-	# 		a.put()
-
-	# 		self.redirect("/")
-
-	# 	else:
-	# 		error = "we need both a title and some artwork!"
-	# 		self.render_front(title, art, error)
-
 class NewPost(Handler):
 	def render_newpost(self, title="", art="", error=""):
-		# arts = db.GqlQuery("SELECT * FROM Blog "
-		# 					"ORDER BY created DESC")
-
 		self.render("newpost.html", title=title, art=art, error=error)
 
 	def get(self):
@@ -79,17 +55,9 @@ class NewPost(Handler):
 			self.render_newpost(title, art, error)
 
 class Confirm(webapp2.RequestHandler):
-#	def render_confirm(self, title="", art=""):
-#		arts = db.GqlQuery("SELECT * FROM Blog "
-#							"WHERE id == obj.key().id()")
-
-#		self.render("confirm.html", title = title, art = art)
-
 	def get(self):
 		arts = Art()
 		arts.sent = self.request.get('')
-#		self.render_confirm()
-
 
 app = webapp2.WSGIApplication([('/', MainPage),
 								('/newpost', NewPost),
